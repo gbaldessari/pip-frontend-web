82425bbae0ada924e47390e91b41ba1c8b240b84
inti cerda <inti.cerda@alumnos.ucn.cl>
2024-11-12 02:12:44 -0300

    Enhance Vite configuration and add student-related services with API integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vite.dev/config/
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      APP_ENV: JSON.stringify(env.APP_ENV)
+    },
+    plugins: [react()],
+  }
+},
+)
